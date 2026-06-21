a0175927cf902c4f43266d017864ca8d3e432764
brooklyng3 <brooklyng3.uni@gmail.com>
2026-06-22 01:23:37 +0700

    modify: clean the UI for students

--- views/partials/header.php
+++ views/partials/header.php
@@ -27,24 +27,25 @@ $pageTitle = $pageTitle ?? 'System & Users';
         </button>
         <div class="collapse navbar-collapse" id="navMain">
             <ul class="navbar-nav me-auto">
-                <li class="nav-item"><a class="nav-link" href="<?= e(url('users', 'index')) ?>">Users</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?= e(url('student_profiles', 'index')) ?>">Student Profiles</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?= e(url('staff_profiles', 'index')) ?>">Staff Profiles</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?= e(url('violation_records', 'index')) ?>">Violation Records</a></li>
+                <!-- Links available to everyone -->
                 <li class="nav-item"><a class="nav-link" href="<?= e(url('scholarship_programs', 'index')) ?>">Scholarship Programs</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?= e(url('scholarship_tiers', 'index')) ?>">Tiers</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?= e(url('eligibility_rules', 'index')) ?>">Rules</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?= e(url('applications', 'index')) ?>">Applications</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?= e(url('scholarship_decisions', 'index')) ?>">Decisions</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?= e(url('scoring_criteria', 'index')) ?>">Criteria</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?= e(url('evaluation_scores', 'index')) ?>">Scores</a></li>
-                <li class="nav-item"><a class="nav-link" href="<?= e(url('application_documents', 'index')) ?>">App Documents</a></li>
+
+                <!-- Administrative Links (Hidden from Students) -->
                 <?php if (isset(current_user()['role']) && in_array(current_user()['role'], ['admin', 'reviewer', 'staff'], true)): ?>
+                    <li class="nav-item"><a class="nav-link" href="<?= e(url('users', 'index')) ?>">Users</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= e(url('student_profiles', 'index')) ?>">Student Profiles</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= e(url('staff_profiles', 'index')) ?>">Staff Profiles</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= e(url('violation_records', 'index')) ?>">Violation Records</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= e(url('scholarship_tiers', 'index')) ?>">Tiers</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= e(url('eligibility_rules', 'index')) ?>">Rules</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= e(url('applications', 'index')) ?>">Applications</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= e(url('scholarship_decisions', 'index')) ?>">Decisions</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= e(url('scoring_criteria', 'index')) ?>">Criteria</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= e(url('evaluation_scores', 'index')) ?>">Scores</a></li>
+                    <li class="nav-item"><a class="nav-link" href="<?= e(url('application_documents', 'index')) ?>">App Documents</a></li>
                     <li class="nav-item">
                         <a class="nav-link <?= (isset($_GET['controller']) && $_GET['controller'] === 'dashboard') ? 'active' : '' ?>" 
-                        href="<?= e(url('dashboard', 'index')) ?>">
-                            <i class="bi bi-graph-up-arrow"></i> Statistics Dashboard
-                        </a>
+                        href="<?= e(url('dashboard', 'index')) ?>">Statistics Dashboard</a>
                     </li>
                 <?php endif; ?>
             </ul>
